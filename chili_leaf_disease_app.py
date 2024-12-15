4cc45fb327620868a9e7b8009eb6f4d70038a0a7
Fatihannisa Listy Zulmi_034 <114486927+Fatihannisa@users.noreply.github.com>
2024-12-15 22:18:23 +0700

    chili_leaf_disease_app.py

--- chili_leaf_disease_app.py
+++ chili_leaf_disease_app.py
@@ -49,7 +49,7 @@ def predict_image(model, image):
 class_names = ["Sehat", "Daun Keriting", "Bercak Daun", "Kutu Kebul", "Kekuningan"] 
 
 # Mulai aplikasi Streamlit
-st.title("Deteksi Penyakit Tanaman Cabai 🌶️")
+st.title("🌶️Deteksi Penyakit Tanaman Cabai")
 st.write("Unggah gambar daun cabai untuk mendeteksi apakah daun tersebut sehat atau memiliki penyakit.")
 
 # Upload gambar
